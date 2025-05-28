39d2cbd11a1367073b54d3a3d2b8f3f2c59f28a1
Dimitrios Mezes <jimezes@gmail.com>
2025-05-28 08:19:52 +0300

    fetch categories on boot and save them to db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Category;
+use App\Services\OpenLibraryService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        $categoriesCount = Category::count();
+        if($categoriesCount == 0){//fetch categories from API if not fetched and save them to database
+             $service = new OpenLibraryService();
+             $service->fetchCategories();
+        }
     }
 }
